5e3985673f94779f448948758f45575a4e7b30e0
Trevor Martin <trevor.j.martin@gmail.com>
2020-10-11 21:55:34 -0400

    removed temporary button

--- src/components/Reminder/reminder.jsx
+++ src/components/Reminder/reminder.jsx
@@ -3,28 +3,29 @@ import Button from "../Button";
 import "./reminder.less";
 
 const Reminder = ({ escape }) => {
+  const handleYes = () => {
+    console.log("yes");
+    escape();
+  };
+  const handleNo = () => {
+    console.log("no");
+    escape();
+  };
   return (
     <div className="ModalTitle">
-      <button
-        style={{ width: "2rem", position: "relative", top: 0 }}
-        onClick={escape}
-      >
-        x
-      </button>
       <h2>IT'S EASY TO FORGET</h2>
       <>
-        <p>May we send you daily text</p>
-        <p>reminders to pray the Memorare?</p>
+        <p>May we send you daily text reminders to pray the Memorare?</p>
         <div className="container-YesNo">
           <Button
             buttonClass="Button-Blue-half"
             text="yes, please"
-            handleClick={() => console.log("yes")}
+            handleClick={handleYes}
           />
           <Button
             buttonClass="Button-Red-half"
             text="no thank you"
-            handleClick={() => console.log("no")}
+            handleClick={handleNo}
           />
         </div>
       </>
