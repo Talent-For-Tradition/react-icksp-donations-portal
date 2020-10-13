8882f3f6e084f91c94bbc97bada100400691bd3e
Trevor Martin <trevor.j.martin@gmail.com>
2020-10-13 19:08:32 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { PrayerCard, AddressForm, MobileTextForm, ThankYou, Donations } from "./components";
-// import "./App.css";
 
 function App() {
   return (
