bf818c0d91f7d2c8a7993e45535ea77f6a436192
Trevor Martin <trevor.j.martin@gmail.com>
2020-10-14 16:14:06 -0400

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("includes our Lord Jesus Christ", async function() {
+  try {
+    const {getByText} = await render(<App />, {wrapper: MemoryRouter});
+    expect(getByText(/our Lord Jesus Christ/i)).toBeInTheDocument();
+  } catch {
+    throw(Error('bad test?'))
+  }
 });
+
+test("logo exists", async function() {
+  try {
+    const {getByAltText} = await render(<App />, {wrapper: MemoryRouter});
+    expect(getByAltText(/logo/i)).toBeInTheDocument();
+  } catch {
+    throw(Error('bad test?'))
+  }
+})
+
+test("logo source file is crest.png", async function() {
+  try {
+    const {getByAltText} = await render(<App />, {wrapper: MemoryRouter});
+    expect(getByAltText(/logo/i).src.split('/').pop()).toEqual('crest.png')
+  } catch {
+    throw(Error())
+  }
+})
+
