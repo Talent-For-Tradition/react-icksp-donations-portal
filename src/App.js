12166e73ce02edefb1732c5a9cd4a1e836ead1bf
Trevor Martin <trevor.j.martin@gmail.com>
2020-11-09 17:46:19 -0500

    test protected routes on CI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { PrayerCard, AddressForm, MobileTextForm, ThankYou, Donations, Checkout } from "./components";
-// import ProtectedRoute from "./auth/protected-route";
+import ProtectedRoute from "./auth/protected-route";
 
 import Profile from './components/Profile';
 
@@ -12,12 +12,12 @@ function App() {
         <img src="/crest.png" className="App-logo" alt="logo" />
       </header>
       <Switch>
-        <Route path="/where"  component={AddressForm} exact={true}/>
-        <Route path="/mobile"  component={MobileTextForm} exact={true}/>
-        <Route path="/thankyou"  component={ThankYou} exact={true}/>
-        <Route path="/donate"  component={Donations} exact={true}/>
-        <Route path="/checkout" component={() => <Checkout open={true} close={()=>console.log('todo')}/>} exact={true}/>
-        <Route path="/profile"  component={Profile} exact={true}/>
+        <ProtectedRoute path="/where"  component={AddressForm} exact={true}/>
+        <ProtectedRoute path="/mobile"  component={MobileTextForm} exact={true}/>
+        <ProtectedRoute path="/thankyou"  component={ThankYou} exact={true}/>
+        <ProtectedRoute path="/donate"  component={Donations} exact={true}/>
+        <ProtectedRoute path="/checkout" component={() => <Checkout open={true} close={()=>console.log('todo')}/>} exact={true}/>
+        <ProtectedRoute path="/profile"  component={Profile} exact={true}/>
         <Route path="/"  component={PrayerCard} exact={true}/>
       </Switch>
     </div>
