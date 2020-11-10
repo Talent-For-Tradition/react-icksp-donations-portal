d42a6d1175b8b011aaf4847c2e2f69d16a74158d
Trevor Martin <trevor.j.martin@gmail.com>
2020-11-10 01:34:59 -0500

    process.env.DATABASE

--- src/integrations/api/payments/knexfile.js
+++ src/integrations/api/payments/knexfile.js
@@ -1,10 +1,10 @@
 // Update with your config settings.
+require("dotenv").config("../../.env");
 
 module.exports = {
-
   development: {
     client: "pg",
-    connection: "postgresql://icksp:logos@localhost:5432/icksp",
+    connection: process.env.DATABASE,
     pool: {
       min: 2,
       max: 10
@@ -12,41 +12,37 @@ module.exports = {
     migrations: {
       directory: "./database/migrations",
       tableName: "knex_migrations"
-    },
+    }
     // seeds: {
     //   directory: "./database/seeds"
     // }
   },
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: process.env.DATABASE,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./database/migrations",
+      tableName: "knex_migrations"
     }
   }
-
 };
